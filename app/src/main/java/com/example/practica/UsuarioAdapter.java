84f80704fc6f679d4810e0a37dd47677f5691e97
FranAO <leoavarooliva@gmail.com>
2025-08-14 10:41:46 -0400

    Clase 14/08

--- app/src/main/java/com/example/practica/UsuarioAdapter.java
+++ app/src/main/java/com/example/practica/UsuarioAdapter.java
@@ -1,9 +1,11 @@
 package com.example.practica;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,19 +14,31 @@ import java.util.List;
 
 public class UsuarioAdapter extends ArrayAdapter<Usuario>
 {
-     private Context context;
-     private List<Usuario> listaUsuario;
+    private Context context;
+    private List<Usuario> listaUsuarios;
 
-    public UsuarioAdapter(Context context, List<Usuario> listaUsuario)
-    {
-        super(context, 0, listaUsuario);
+    public UsuarioAdapter(Context context, List<Usuario> listaUsuarios) {
+        super(context, 0, listaUsuarios);
         this.context = context;
-        this.listaUsuario = listaUsuario;
+        this.listaUsuarios = listaUsuarios;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        if(convertView == null)
+        {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_usuario, parent, false);
+        }
+        Usuario usuario = listaUsuarios.get(position);
+
+        TextView tvNombre = convertView.findViewById(R.id.tvNombre);
+        TextView tvEmail = convertView.findViewById(R.id.tvEmail);
+
+        tvNombre.setText(usuario.getNombre());
+        tvEmail.setText(usuario.getEmail());
+
+        return convertView;
     }
 }
